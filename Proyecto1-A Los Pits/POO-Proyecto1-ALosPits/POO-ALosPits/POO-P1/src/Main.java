3ce00ba7c7c562c766f3dbfce1119077716fd941
luisale06 <86024351+luisale06@users.noreply.github.com>
2022-12-12 23:03:40 -0600

    Clase para registro de Marca lista

--- Proyecto1-A Los Pits/POO-Proyecto1-ALosPits/POO-ALosPits/POO-P1/src/Main.java
+++ Proyecto1-A Los Pits/POO-Proyecto1-ALosPits/POO-ALosPits/POO-P1/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         Marca marca = new Marca();
 
-        marca.add("BMW");
-        //marca.delete("Audi");
+        marca.add("Mercedes Benz", "SUV");
+
+        //marca.delete("Audi", "Sedan");
     }
 }
--- Proyecto1-A Los Pits/POO-Proyecto1-ALosPits/POO-ALosPits/POO-P1/src/Marca.java
+++ Proyecto1-A Los Pits/POO-Proyecto1-ALosPits/POO-ALosPits/POO-P1/src/Marca.java
@@ -1,27 +1,93 @@
 import java.io.*;
-
+import java.util.List;
 
 public class Marca {
 
-    String file = "marcas.csv", tipo = "4x4";
+    File file = new File("marcas.csv");
+    File temp_file = new File("temp.csv");
+    String lineas_archivo;
     FileWriter fw;
     BufferedWriter bw;
     PrintWriter pw;
+    BufferedReader lector;
 
-    public void add(String marca) {
+    public void add(String marca, String tipo) throws IOException {
+        boolean ismarca = true;
         try{
+            lector = new BufferedReader(new FileReader(file));
+
+            while ((lineas_archivo = lector.readLine()) != null){
+                String[] fila = lineas_archivo.split(",");
+                if (fila[0].equals(marca) && fila[1].equals(tipo)) {
+                    ismarca = false;
+                    break;
+                }
+            }
+        }
+        catch(Exception e) {e.printStackTrace();}
+        finally {lector.close();}
+
+        if (ismarca){
             fw = new FileWriter(file, true);
             bw = new BufferedWriter(fw);
             pw = new PrintWriter(bw);
             pw.println(marca + "," + tipo);
             pw.flush();
             pw.close();
-            System.out.println(marca + " Añadida");
+            System.out.println(marca + "," + tipo + " Añadida");
         }
-        catch(Exception e) {e.printStackTrace();}
+        else {
+            System.out.println(marca + "," + tipo + " NO añadida");
+        }
     }
 
-    public void delete(String marca) {
-        System.out.println(marca + " Eliminada");
+    public void delete(String marca, String tipo) throws IOException {
+        try{
+            fw = new FileWriter(temp_file, true);
+            bw = new BufferedWriter(fw);
+            pw = new PrintWriter(bw);
+            boolean isdeleted = false;
+
+            lector = new BufferedReader(new FileReader(file));
+            while ((lineas_archivo = lector.readLine()) != null){
+                String[] columnas = lineas_archivo.split(",");
+                if (!columnas[0].equals(marca) || !columnas[1].equals(tipo)) {
+                    pw.println(lineas_archivo);
+                }
+                else {
+                    isdeleted = true;
+                }
+            }
+
+            pw.flush();
+            pw.close();
+
+            FileInputStream in = new FileInputStream(temp_file);
+            FileOutputStream out = new FileOutputStream(file);
+            try{
+                int n;
+
+                while ((n = in.read()) != -1){
+                    out.write(n);
+                }
+            }finally{
+                if (in != null){
+                    in.close();
+                }
+                if (out != null){
+                    out.close();
+                }
+            }
+            temp_file.delete();
+
+            if (isdeleted == true){
+                System.out.println(marca + "," + tipo + " Eliminada");
+            }
+            else{
+                System.out.println(marca + "," + tipo + " NO Eliminada");
+            }
+        }
+        catch(Exception e) {e.printStackTrace();}
+        finally {lector.close();}
     }
 }
