adbdb5892c4f399f1d752b66c4e97ae824bf1785
NotFabianML <fabianmloaiza@gmail.com>
2022-12-14 09:05:08 -0600

    GUI Login y Clase Usuario

--- Proyecto1-A Los Pits/POO-Proyecto1-ALosPits/POO-ALosPits/POO-P1/src/Main.java
+++ Proyecto1-A Los Pits/POO-Proyecto1-ALosPits/POO-ALosPits/POO-P1/src/Main.java
@@ -2,10 +2,14 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        new Login();
         Marca marca = new Marca();
+        Usuario usuario = new Usuario();
 
-        marca.add("Mercedes Benz", "SUV");
+        //marca.add("Mercedes Benz", "SUV");
 
         //marca.delete("Audi", "Sedan");
+
+        //usuario.add("Fabian", "abc123");
     }
 }
